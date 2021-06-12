e98800d563d2b6f181c78d8bc6c689891de468f4
savelij13 <savelij13@yandex.ru>
2021-06-12 14:19:38 +0300

    Собираемые файлы сохраняются в папку bin;

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 
-@ECHO OFF
+@echo off
 
 ..\..\yad\svn\pentevo\tools\asw\bin\asw -U -L -x -olist lst\miniboot.lst -D ADRLOAD=0x4000 miniboot.a80
 ..\..\yad\svn\pentevo\tools\asw\bin\p2bin miniboot.p rom/miniboot.rom -r $-$ -k
@@ -12,6 +12,6 @@ rem copy /Y /B rom\miniboot.rom D:\yad\unrealspeccy\miniboot.rom
 ..\..\yad\svn\pentevo\tools\asw\bin\p2bin make_scl.p rom/make_scl.rom -r $-$ -k
 
 ..\..\yad\svn\pentevo\tools\csum32\csum32 rom\make_scl.rom
-copy /B /Y rom\make_scl.rom+csum32.bin minibootsd.scl
+copy /B /Y rom\make_scl.rom+csum32.bin bin\minibootsd.scl
 
 del *.bin
